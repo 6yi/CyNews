3f7be4610a91a94ed5fa5837c872183009109b7f
Fake <1596136105@qq.com>
2020-11-20 23:52:31 +0800

    login

--- cy-news-client/src/containers/Login/login.jsx
+++ cy-news-client/src/containers/Login/login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { List, InputItem,Icon,Button } from 'antd-mobile';
-import './login.less'
+import login from './login.module.css'
 export default class Login extends Component {
     constructor() {
         super()
@@ -9,30 +9,30 @@ export default class Login extends Component {
             user: {
                 u_username: '',
                 u_password: '',
-                u_nickname: '',
+                
             }
         }
     }
 
     render() {
         return (
-            <div className='background'>
-               <div className='cancleicon'>
+            <div className= {login.background}>
+               <div className= {login.cancleicon}>
                 <Icon type='cross' size='lg'/>
                </div>
-               <div className='Formdata'>
-                <span className='logintitle'>账号密码登录</span>
-                <List className='list'>
-                    <List.Item className='listItem'>
-                    <input type="text" placeholder='请输入账号' className='input'/>
+               <div className={login.Formdata}>
+                <span className={login.logintitle}>账号密码登录</span>
+                <List className={login.list}>
+                    <List.Item className={login.listItem}>
+                    <input type="text" placeholder='请输入账号' className={login.input}/>
                     </List.Item>
-                    <List.Item className='listItem'>
-                    <input type="text" placeholder='请输入密码' className='input'/>
+                    <List.Item className={login.listItem}>
+                    <input type="text" placeholder='请输入密码' className={login.input}/>
                     </List.Item>
-                    <Button className="button">登录</Button>
+                    <Button className={login.button}>登录</Button>
                 </List>
                </div>
-               <div className="footer">
+               <div className={login.footer}>
                   
                     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605717095531&di=8ea976b885c0d7783c402b2053f4ba41&imgtype=0&src=http%3A%2F%2Fimgcdn1.gzhtedu.cn%2Fimages%2Fxiaohui%2F20161124034151-27.jpg" alt=""/>
                    <p>城院新闻</p>
--- cy-news-client/src/containers/Register/register.jsx
+++ cy-news-client/src/containers/Register/register.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { List, InputItem,Icon,Button } from 'antd-mobile';
-import './register.less'
+import register from './register.module.css'
+import '../../assets/css/base.css'
 export default class Login extends Component {
     constructor() {
         super()
@@ -9,33 +10,45 @@ export default class Login extends Component {
             user: {
                 u_username: '',
                 u_password: '',
-                u_nickname: '',
+                u_affirmpassword: '',
             }
         }
+    }
+    inputOnChange(e,value) {
+        /* this.setState({
+            [field]:value
+        }) */
+        console.log(e,value);
     }
 
     render() {
         return (
-            <div className='background'>
-               <div className='cancleicon'>
+            <div className={register.background}>
+               <div className={register.cancleicon}>
                 <Icon type='cross' size='lg'/>
                </div>
-               <div className='Formdata'>
-                <span className='logintitle'>账号密码登录</span>
-                <List className='list'>
-                    <List.Item className='listItem'>
-                    <input type="text" placeholder='请输入账号' className='input'/>
+               <div className={register.Formdata}>
+                <span className={register.logintitle}>注册</span>
+                <List className={register.list}>
+                    <List.Item className={register.listItem}>
+                <input type="text" placeholder='请输入账号' className={register.input}  value={this.state.user.u_username} onChange={(e) => {this.inputOnChange(this.user.u_username,e)}}/>
                     </List.Item>
                     <List.Item className='listItem'>
-                    <input type="text" placeholder='请输入密码' className='input'/>
+                    <input type="text" placeholder='请输入密码' className={register.input}  value={this.state.user.u_password}/>
                     </List.Item>
-                    <Button className="button">登录</Button>
+                    <List.Item>
+                    <input type="text" placeholder='请再次输入密码' className={register.input}  value={this.state.user.u_affirmpassword}/>
+                    </List.Item>
+                    <Button className={register.button}>登录</Button>
                 </List>
                </div>
-               <div className="footer">
+               <div className={register.footer}>
                   
                     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605717095531&di=8ea976b885c0d7783c402b2053f4ba41&imgtype=0&src=http%3A%2F%2Fimgcdn1.gzhtedu.cn%2Fimages%2Fxiaohui%2F20161124034151-27.jpg" alt=""/>
                    <p>城院新闻</p>
+
+
+                   <p className={register.form}>注册即视为同意《城院新闻服务及隐私条款》</p>
                 
                </div>
             </div>
