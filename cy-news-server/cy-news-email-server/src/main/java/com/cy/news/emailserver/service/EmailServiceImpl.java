729c52399f0d31f662f25ab5b0f68f86463bbf22
yj <979399417@qq.com>
2020-11-21 23:06:20 +0800

    mail_active

--- cy-news-server/cy-news-email-server/src/main/java/com/cy/news/emailserver/service/EmailServiceImpl.java
+++ cy-news-server/cy-news-email-server/src/main/java/com/cy/news/emailserver/service/EmailServiceImpl.java
@@ -6,6 +6,7 @@ import com.cy.news.api.service.EmailService;
 import com.cy.news.emailserver.utils.EmailUtils;
 import com.cy.news.pojo.DTO.ResultDTO;
 import com.cy.news.pojo.Exception.EmailRetErrorCode;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,15 +17,16 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@DubboService(version = "1.0.0")
 public class EmailServiceImpl implements EmailService {
 
      @Value("${spring.mail.username}")
-    private   String sender="yj979399417@163.com";
+    private   String sender;//发件人
 
     @Value("${spring.mail.title}")
-    private String title="点击链接验证邮箱完成注册";
+    private String title;
 
-    @Value("${spring.mail.addr")
+    @Value("${spring.mail.addr}")
     private String addr;
 
 
@@ -37,18 +39,15 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public ResultDTO SendEmail(Integer id, String email) {
-     //   String title="点击链接验证邮箱完成注册";
 
-      //  String sender="yj979399417@163.com";
-        String rand= RandomUtil.randomString(8);
+        String rand= RandomUtil.randomString(8);//随机生成数
 
         String text="请点击 "+addr+"?id="+id+"&"+"code="+rand;
 
-        redisTemplate.opsForValue().set(id.toString(),rand,5, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().set(id.toString(),rand,5, TimeUnit.MINUTES);//使用redis存储验证码跟id信息
 
-        String timeOutValue = redisTemplate.opsForValue().get(id.toString());
 
-        emailUtils.send(sender,email,title,text);
+        emailUtils.send(sender,email,title,text);//发送邮件
 
         return  ResultDTO.builder().code(EmailRetErrorCode.OK).build();
 
