70b3d546c1c60382dfaa756c68b0f7f8ac4817ff
yj <979399417@qq.com>
2020-11-24 18:35:45 +0800

    update

--- cy-news-server/cy-news-pojo/src/main/java/com/cy/news/pojo/Exception/GlobalExceptionHandler.java
+++ cy-news-server/cy-news-pojo/src/main/java/com/cy/news/pojo/Exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cy.news.pojo.Exception;
 
 import com.cy.news.pojo.DTO.ResultDTO;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,8 +18,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 
+
     @ExceptionHandler(value = RuntimeException.class)
     public ResultDTO error(RuntimeException e){
+        //把开发者邮箱拉出来，把异常信息全部发送到邮箱
         
         return ResultDTO.builder().code(500).data("网络连接异常").build();
 
