faaba1100e232f56edcc4c393af3d4f9a12514be
Fake <1596136105@qq.com>
2020-12-04 14:11:19 +0800

    新闻内容界面完善

--- cy-news-client/src/containers/NewsContent/TabBar/tabbar.jsx
+++ cy-news-client/src/containers/NewsContent/TabBar/tabbar.jsx
@@ -1,13 +1,40 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import tabbar from './tabbar.module.css'
+import { Badge } from 'antd-mobile'
+import editImge from '../../../assets/img/edit.png'
+import commentImge from '../../../assets/img/comment.png'
+import likeImage from '../../../assets/img/like.png'
+import collectImage from '../../../assets/img/collect.png'
+import shareImage from '../../../assets/img/share.png' 
+import './tabbar.less'
+
 export default class NewsTabBar extends Component {
     render() {
-        return(
+        return (
             <div className={tabbar.box}>
                 <div className={tabbar.input}>
-                    111
+                    <img className={tabbar.img} src={editImge} alt="" />
+                    <span className={tabbar.pac}>说两句</span>
+                </div>
+
+                <div className={tabbar.comment +' comment'}>
+                    <img src={commentImge} alt="" />
+                </div>
+
+                <div className={tabbar.like}>
+                    <Badge text={99}>
+                        <img src={likeImage} alt="" />
+                    </Badge>
+                </div>
+
+                <div className={tabbar.collect}>
+                    <img src={collectImage} alt="" />
+                </div>
+
+                <div className={tabbar.share}>
+                    <img src={shareImage} alt="" />
                 </div>
             </div>
         )
-    }    
+    }
 }
